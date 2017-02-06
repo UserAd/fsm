f361ed24dc665a69a0e77204855f30be55e2eb14
Konstantin Tumalevich <userad@gmail.com>
2017-02-06 16:37:36 +0500

    Readme and tests

--- spec/fsm_spec.rb
+++ spec/fsm_spec.rb
@@ -1,11 +1,60 @@
 require "spec_helper"
 
-describe Fsm do
-  it "has a version number" do
-    expect(Fsm::VERSION).not_to be nil
+describe FSM do
+
+  before(:each) do
+    @machine = FSM.new(:pending)
+    @machine.when(:confirm, pending: :confirmed)
+    @machine.when(:ignore, pending: :ignored)
+    @machine.when(:reset, confirmed: :pending, ignored: :pending, tested: :pending)
+    @machine.when(:test_star, :* => :tested)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "returns an array with the defined events" do
+    expect(@machine.events).to eq([:confirm, :ignore, :reset, :test_star])
   end
+
+  it "returns an array with the defined states" do
+    expect(@machine.states).to eq([:pending, :confirmed, :ignored, :tested, :*])
+  end
+
+  it "changes the state if transition is possible" do
+    expect(@machine.trigger?(:confirm)).to eq(true)
+    expect(@machine.trigger(:confirm)).to eq(true)
+    expect(@machine.state).to eq(:confirmed)
+  end
+
+  it "not changes the state if transition is not possible" do
+    expect(@machine.trigger?(:reset)).to eq(false)
+    expect(@machine.trigger(:reset)).to eq(false)
+    expect(@machine.state).to eq(:pending)
+  end
+
+  it "Fire event when state changed" do
+    new_event_name = nil
+    @machine.on :state_changed do |event_name, changeset|
+      new_event_name = event_name
+    end
+    @machine.trigger(:confirm)
+    expect(new_event_name).to eq(:confirm)
+  end
+
+  it "Fire named event when state changed" do
+    new_state = nil
+    @machine.on :confirmed do |state|
+      new_state = state
+    end
+    @machine.trigger(:confirm)
+    expect(new_state).to eq(:confirm)
+  end
+
+  it "Changes state to tested if :test_star fired" do
+    new_state = nil
+    @machine.on :state_changed do |_event, state|
+      new_state = state[:to]
+    end
+    @machine.trigger(:test_star)
+    expect(new_state).to eq(:tested)
+  end
+
 end
